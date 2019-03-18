d5de3ecbdb0e7c32c2e07e035ee4bbbf06a2bfaa
Casa Taloyum <casatwy@msn.com>
2019-03-18 23:00:11 +0800

    modification

---
+++ APITemplateProject_OC/APITemplateProject_OC/Service/__ServiceName__.h
@@ -0,0 +1,18 @@
+//
+//  __ServiceName__.h
+//  APITemplateProject_OC
+//
+//  Created by casa on 2019/3/18.
+//  Copyright © 2019 casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CTNetworking/CTNetworking.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface __ServiceName__ : ctservicepro
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ APITemplateProject_OC/APITemplateProject_OC/Service/__ServiceName__.m
@@ -0,0 +1,13 @@
+//
+//  __ServiceName__.m
+//  APITemplateProject_OC
+//
+//  Created by casa on 2019/3/18.
+//  Copyright © 2019 casa. All rights reserved.
+//
+
+#import "__ServiceName__.h"
+
+@implementation __ServiceName__
+
+@end
